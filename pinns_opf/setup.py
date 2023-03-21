a1aed33c00be39046f1eea203f061b98beb524e9
EVDIO <evangelos.dio@gmail.com>
2023-03-21 19:36:25 +0100

    Create Structure

---
+++ pinns_opf/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Physics informed policies for smart grid operators',
+    author='edi',
+    license='MIT',
+)
